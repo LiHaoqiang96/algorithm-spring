5d5a978c8108e5c40b37056af67b6f0f4cade51a
lihaoqiang <lihaoqiang001@ke.com>
2019-02-13 15:52:33 +0800

    Create

---
+++ src/main/java/com/xust/algorithmspring/AlgorithmSpringApplication.java
@@ -0,0 +1,14 @@
+package com.xust.algorithmspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmSpringApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/xust/algorithmspring/AlgorithmSpringApplicationTests.java
@@ -0,0 +1,17 @@
+package com.xust.algorithmspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlgorithmSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
